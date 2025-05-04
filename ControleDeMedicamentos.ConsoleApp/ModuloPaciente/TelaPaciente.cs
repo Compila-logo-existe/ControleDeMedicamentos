d6666ffe2d4b7d1655ecfb257b0b4eed4e304f4e
kyonav <tokyonav@proton.me>
2025-05-04 13:44:04 -0300

    Refact: implemented abstract method TemRestricoesNoInserir()

--- ControleDeMedicamentos.ConsoleApp/ModuloPaciente/TelaPaciente.cs
+++ ControleDeMedicamentos.ConsoleApp/ModuloPaciente/TelaPaciente.cs
@@ -1,8 +1,30 @@
-using ControleDeMedicacao.ConsoleApp.Compartilhado;
+using ControleDeMedicamentos.ConsoleApp.Compartilhado;
 
-namespace ControleDeMedicacao.ConsoleApp.ModuloPaciente;
+namespace ControleDeMedicamentos.ConsoleApp.ModuloPaciente;
 
-protected class TelaPaciente : 
+internal class TelaPaciente : TelaBase<Paciente>, ITelaCrud
 {
+    private IRepositorioPaciente RepositorioPaciente { get; set; } 
+
+    internal TelaPaciente(IRepositorioPaciente repositorio) : base("Paciente", repositorio)
+    {
+        RepositorioPaciente = repositorio;
+    }
+
+    public override bool TemRestricoesNoInserir(Paciente paciente, out string mensagem)
+    {
+        mensagem = string.Empty; 
+        
+        if (RepositorioPaciente.VerificarCartaoSUSRegistros(paciente))
+        {
+            mensagem += "\nJa existe um cadastro com esse Cartao do SUS!";   
+
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
 
 }
