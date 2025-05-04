4fb997c2e025fe995ccd4b8c8b06fb896935ca3b
kyonav <tokyonav@proton.me>
2025-05-04 12:13:09 -0300

    Feat: added constructor and mandatory methods

---
+++ ControleDeMedicamentos.ConsoleApp/ModuloPaciente/RepositorioPacienteEmArquivo.cs
@@ -0,0 +1,18 @@
+using ControleDeMedicamentos.ConsoleApp.Compartilhado;
+
+namespace ControleDeMedicamentos.ConsoleApp.ModuloPaciente;
+
+internal class RepositorioPacienteEmArquivo : RepositorioBaseEmArquivo<Paciente>, IRepositorioPaciente
+{
+    protected RepositorioPacienteEmArquivo(ContextoDados contexto) : base(contexto) {}
+
+    public bool VerificarCartaoSUSRegistros(Paciente paciente)
+    {
+        return contexto.Pacientes.Any(p => p != null && p.CartaoSUS == paciente.CartaoSUS);
+    }
+
+    protected override List<Paciente> ObterRegistros()
+    {
+        return contexto.Pacientes; 
+    }
+}
