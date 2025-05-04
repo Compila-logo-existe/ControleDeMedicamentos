8fee8f4be75d877013b4b6e03509bbabda9a0258
kyonav <tokyonav@proton.me>
2025-05-04 16:36:36 -0300

    Refact: implemented abstract method ExibirCabecalhoTabela()

--- ControleDeMedicamentos.ConsoleApp/ModuloPrescricaoMedica/TelaPrescricaoMedica.cs
+++ ControleDeMedicamentos.ConsoleApp/ModuloPrescricaoMedica/TelaPrescricaoMedica.cs
@@ -11,5 +11,13 @@ internal class TelaPrescricaoMedica : TelaBase<PrescricaoMedica>, ITelaCrud
         RPMEA = rPMEA; 
     }
 
+    protected override void ExibirCabecalhoTabela()
+    {
+        Console.WriteLine
+        (
+            "{0, -10} | {1, -10} | {2, -15} | {3, -20}",
+            "ID", "CRMMedico", "Data Emissao", "Medicamentos"
+        );
+    }
 }
 
