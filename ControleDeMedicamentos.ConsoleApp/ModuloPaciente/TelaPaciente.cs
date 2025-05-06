d056a317992be6fd4c32685fc34aa304d1ed277f
kyonav <tokyonav@proton.me>
2025-05-06 06:50:46 -0300

    Refact: edited TelaPaciente access modifier

--- ControleDeMedicamentos.ConsoleApp/ModuloPaciente/TelaPaciente.cs
+++ ControleDeMedicamentos.ConsoleApp/ModuloPaciente/TelaPaciente.cs
@@ -1,25 +1,35 @@
 using ControleDeMedicamentos.ConsoleApp.Compartilhado;
+using ControleDeMedicamentos.ConsoleApp.ModuloPrescricaoMedica;
 
 namespace ControleDeMedicamentos.ConsoleApp.ModuloPaciente;
 
-internal class TelaPaciente : TelaBase<Paciente>
+public class TelaPaciente : TelaBase<Paciente>
 {
-    private IRepositorioPaciente IRepositorioPaciente { get; set; } 
+    private RepositorioPacienteEmArquivo RepositorioPacienteEmArquivo { get; set; } 
+    
+    private TelaPrescricaoMedica TelaPrescricaoMedica { get; set; }
+    private RepositorioPrescricaoMedicaEmArquivo RepositorioPrescricaoMedicaEmArquivo { get; set; }
 
     internal TelaPaciente
     (
-        IRepositorioPaciente iRepositorioPaciente
+        RepositorioPacienteEmArquivo repositorioPacienteEmArquivo,
 
-    ) : base("Paciente", iRepositorioPaciente)
+        RepositorioPrescricaoMedicaEmArquivo repositorioPrescricaoMedicaEmArquivo,
+        TelaPrescricaoMedica telaPrescricaoMedica
+
+    ) : base("Paciente", repositorioPacienteEmArquivo)
     {
-        IRepositorioPaciente = iRepositorioPaciente;
+        RepositorioPacienteEmArquivo = repositorioPacienteEmArquivo;
+
+        RepositorioPrescricaoMedicaEmArquivo = repositorioPrescricaoMedicaEmArquivo;
+        TelaPrescricaoMedica = telaPrescricaoMedica;
     }
 
     public override bool TemRestricoesNoInserir(Paciente paciente, out string mensagem)
     {
         mensagem = string.Empty; 
         
-        if (IRepositorioPaciente.VerificarCartaoSUSRegistros(paciente))
+        if (RepositorioPacienteEmArquivo.VerificarCartaoSUSRegistros(paciente))
         {
             mensagem += "\nJa existe um cadastro com esse Cartao do SUS!";   
 
@@ -41,8 +51,29 @@ internal class TelaPaciente : TelaBase<Paciente>
 
         Console.Write("Digite o Numero do Cartao do SUS: ");
         string cartaoSUS = Console.ReadLine() ?? string.Empty;
-        
-        return new Paciente(nome, telefone, cartaoSUS, null, null); // dxando null de placeholder
+
+        List<PrescricaoMedica> prescricoesMedicas = []; 
+        do 
+        {
+            TelaPrescricaoMedica.VisualizarRegistros(false);
+            Console.Write("Digite o Id da prescricao: ");
+            int idPrescricao = int.Parse(Console.ReadLine()!);  
+            PrescricaoMedica prescricao = RepositorioPrescricaoMedicaEmArquivo.SelecionarRegistroPorId(idPrescricao); 
+            prescricoesMedicas.Add(prescricao);
+
+            Console.Write("Deseja atribuir mais prescricoes? (S/n): ");
+            string opcao = Console.ReadLine()!.ToUpper() ?? string.Empty;
+
+            if (opcao == "S")
+            {
+                continue; 
+            }
+            else if (opcao == "N")
+            {
+                return new Paciente(nome, telefone, cartaoSUS, prescricoesMedicas, null); // dxando null de placeholder
+            }
+        }
+        while(true);
     }
 
     protected override void ExibirCabecalhoTabela()
