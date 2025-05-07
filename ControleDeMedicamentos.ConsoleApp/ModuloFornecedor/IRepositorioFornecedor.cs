b2d8f1a518fa5381ad4e33eaefbb5669b9ac0599
kyonav <tokyonav@proton.me>
2025-05-07 03:01:22 -0300

    Refact: fixed typos

--- ControleDeMedicamentos.ConsoleApp/ModuloFornecedor/IRepositorioFornecedor.cs
+++ ControleDeMedicamentos.ConsoleApp/ModuloFornecedor/IRepositorioFornecedor.cs
@@ -5,6 +5,6 @@ namespace ControleDeMedicamentos.ConsoleApp.ModuloFornecedor;
 public interface IRepositorioFornecedor : IRepositorio<Fornecedor>
 {
     public bool ListaVazia();
-    public bool VerificarCNPJRegistros(Fornecedor fornecedor);
+    public bool VerificarCNPJInserirRegistro(Fornecedor fornecedor);
     public bool VerificarCNPJEditarRegistro(Fornecedor registroEscolhido, Fornecedor dadosEditados);
 }
